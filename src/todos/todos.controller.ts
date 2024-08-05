26b17aa9b3f0f1f61e4dbbeb01dc51fde2c9b38c
faeze <77538218+faezemoradi8993@users.noreply.github.com>
2024-08-05 19:43:04 +0330

    add tag to todos in swagger

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-
+import { ApiTags } from '@nestjs/swagger';
+@ApiTags('todos')
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
