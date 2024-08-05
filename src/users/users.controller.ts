76f1016a0db73f1d5acabb5947b3ea1c37861b4f
faeze <77538218+faezemoradi8993@users.noreply.github.com>
2024-08-05 16:46:55 +0330

    add user controlers (getUsers,getUserById,createUser)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user-dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Get()
+  getUsers(): User[] {
+    return this.usersService.findAll();
+  }
+  @Get(':id')
+  getUserById(@Param('id') id: User['id']): User {
+    //ToDo : auto parse id
+    return this.usersService.findById(Number(id));
+  }
+  @Post('create')
+  createUser(@Body() body: CreateUserDto): User {
+    return this.usersService.createUser(body);
+  }
+}
