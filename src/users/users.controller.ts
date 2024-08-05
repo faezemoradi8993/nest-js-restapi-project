43ca6d72989527cbf7572c23668117a4785d3e13
faeze <77538218+faezemoradi8993@users.noreply.github.com>
2024-08-05 19:18:26 +0330

    add exception 404

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  InternalServerErrorException,
+  NotFoundException,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user-dto';
 import { User } from './entities/user.entity';
 import {
   ApiConflictResponse,
   ApiCreatedResponse,
+  ApiNoContentResponse,
   ApiOkResponse,
   ApiQuery,
   ApiTags,
@@ -21,13 +32,19 @@ export class UsersController {
     return this.usersService.findAll(name);
   }
   @ApiOkResponse({ type: User, isArray: false, description: 'the user' })
+  @ApiNoContentResponse()
   @Get(':id')
-  getUserById(@Param('id') id: User['id']): User {
+  getUserById(@Param('id') id: string): User {
     //ToDo : auto parse id
-    return this.usersService.findById(Number(id));
+    const user = this.usersService.findById(Number(id));
+    if (!user) {
+      throw new NotFoundException();
+      //       https://docs.nestjs.com/exception-filters
+    }
+    return user;
   }
   @ApiCreatedResponse({ type: User })
-  @Post('')
+  @Post()
   createUser(@Body() body: CreateUserDto): User {
     return this.usersService.createUser(body);
   }
