e500d2c7715e51c6ebae3b679cd72ad306ce25ec
faeze <77538218+faezemoradi8993@users.noreply.github.com>
2024-08-05 18:25:20 +0330

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Nest Api')
+    .setDescription('the desctiption of the api')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
   await app.listen(3000);
 }
 bootstrap();
